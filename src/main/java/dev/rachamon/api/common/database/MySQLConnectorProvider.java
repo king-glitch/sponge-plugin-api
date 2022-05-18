204bbb9a21514236daae8b3d2bcac5e457af74fc
king-glitch <william.sief@bumail.net>
2022-05-18 15:26:07 +0700

    add(option): max pool size

--- src/main/java/dev/rachamon/api/common/database/MySQLConnectorProvider.java
+++ src/main/java/dev/rachamon/api/common/database/MySQLConnectorProvider.java
@@ -10,12 +10,12 @@ public class MySQLConnectorProvider implements IDatabaseConnector {
     private HikariDataSource hikari;
     private boolean initializedSuccessfully;
 
-    public MySQLConnectorProvider(String hostname, int port, String database, String username, String password, boolean useSSL) {
+    public MySQLConnectorProvider(String hostname, int port, String database, String username, String password, boolean useSSL, int maxPoolSize) {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=" + useSSL);
         config.setUsername(username);
         config.setPassword(password);
-        config.setMaximumPoolSize(3);
+        config.setMaximumPoolSize(maxPoolSize);
 
         try {
             this.hikari = new HikariDataSource(config);
